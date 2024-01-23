bcb1828ca1b335ecf1248a73e9b12e20a20f7dad
Katie Casamento <kcsmnt0@gmail.com>
2024-01-23 00:05:21 -0800

    add machine-specific debugging scripts to gitignore

--- backend-pocket/debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export $(cat .env | xargs)
-cd litex
-source .venv/bin/activate
-python litex/tools/litex_term.py --jtag-config=openocd_usb_blaster.cfg jtag
--- backend-pocket/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo mount /dev/sdf1 /mnt
-sudo cp -v rust.bin /mnt/Assets/riscv/common/boot.bin
-sudo cp -v data.json /mnt/Cores/agg23.RISCV/data.json
-sudo cp -v interact.json /mnt/Cores/agg23.RISCV/interact.json
-sudo umount /mnt
-sudo sync
